90ebd4342b66cc0bdf76c4adfc7bd82e95226fd0
csakafeda <bako.feodora97@gmail.com>
2023-09-11 13:28:31 +0200

    add spotify auth server

---
+++ server.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken;
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: "http://localhost:5173/music",
+        clientId: "d65b7acbcf89421faada8ab7b50a0752",
+        clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+        refreshToken
+      });
+      spotifyApi.refreshAccessToken()
+      .then((data) => {
+        res.json({
+            accessToken: data.body.access_token,
+            expiresIn: data.body.expires_in
+        })
+      }).catch(() => {
+        res.sendStatus(400);
+      })
+})
+
+app.post('/login', (req, res) => {
+    const code = req.body.code;
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: "http://localhost:5173/music",
+        clientId: "d65b7acbcf89421faada8ab7b50a0752",
+        clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      });
+
+    spotifyApi.authorizationCodeGrant(code)
+        .then(data => {
+            res.json({
+                accessToken: data.body.access_token,
+                refreshToken: data.body.refresh_token,
+                expiresIn: data.body.expires_in
+            });
+        }).catch((err) => {
+            console.error("Error in authorizationCodeGrant:", err);
+            res.sendStatus(400);
+        });
+});
+
+app.listen(process.env.PORT || 3001, () => {
+    console.log('Listening on port 3001');
+});
